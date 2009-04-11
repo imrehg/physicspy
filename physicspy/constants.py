e6f8b9239723ed9583ddb6802ed35be4b033f955
Gergely Imreh <imrehg@gmail.com>
2009-04-11 09:38:36 +0800

    constants module created
    Currently constants are done as functions returning a single
    floating point value, to be able to use them with docstrings.
    Later, when the dimension analysis module is ready, it should
    include returning dimensions.

---
+++ physicspy/constants.py
@@ -0,0 +1,29 @@
+#!/usr/bin/constants
+""" Collection of physical constants
+
+Main source: http://physics.nist.gov/cuu/Constants/index.html
+"""
+
+def AMU():
+    """ Atomic mass unit, kg """
+    return 1.660538782e-27
+
+def AVOGADRO():
+    """ Avogadro constant, mol^-1 """
+    return 6.02214179e23
+
+def BOLTZMANN():
+    """ Boltzmann constant, J K^-1 """
+    return 1.3806504e-23
+
+def ELECTRONMASS():
+    """ Electron mass, kg """
+    return 9.10938215e-31
+
+def ELEMENTARYCHARGE():
+    """ Elementary charge, C """
+    return 1.602176487e-19
+    
+def SPEEDOFLIGHT():
+    """ Speed of light in vacuum, m s^-1 """
+    return 299792458
