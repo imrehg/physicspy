57eb66ce8050efe5b6a5a214893b2d38c5bebf60
Gergely Imreh <imrehg@gmail.com>
2009-04-14 13:55:58 +0800

    Six-j symbol unittest added (incomplete)

--- physicspy/quantum/tests/atomictest.py
+++ physicspy/quantum/tests/atomictest.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 """ Testing the atomic module """
 
+from __future__ import division
 from physicspy.quantum.atomic import *
-from numpy import random, allclose
+from numpy import random, allclose, ones
 import unittest
 
 class TestSequenceFunctions(unittest.TestCase):
@@ -36,6 +37,24 @@ class TestSequenceFunctions(unittest.TestCase):
         J = ([1,1,2])
         M = ([0,1,-1])
         self.failUnless(allclose( (2*J[0]+1)*threej(J[0],J[1],J[2],M[0],M[1],M[2])**2 , 0.3), 'wrong three-j symbol value')
+
+    def testSixJTriangle(self):
+        """ Triangle rule test of six-j symbols"""
+        result = ones(2)
+        J = ([0,1/2,1])
+        L = ([3,3,7/2])
+        result[0] = sixj(J[0],J[1],J[2],L[0],L[1],L[2]) 
+        J = ([2,1/2,1])
+        L = ([3,3,7/2])
+        result[1] = sixj(J[0],J[1],J[2],L[0],L[1],L[2]) 
+        """ Should add the other triangle tests as well """
+        self.failUnless(not any(result[:]), 'six-j symbol vector triangularity check fail')
+
+    def testSixJValue(self):
+        """ Six-J symbol value test by calculating transition strength """
+        J = ([1/2,1/2,1])
+        L = ([3,3,7/2])
+        self.failUnless(allclose( (2*L[0]+1)*(2*J[0]+1)*sixj(J[0],J[1],J[2],L[0],L[1],L[2])**2 , 0.25), 'wrong six-j symbol value')
 
 if __name__ == '__main__':
     unittest.main()
