00698b6d3df0ac08c10a8f7b7189715534f26a40
Gergely Imreh <imrehg@gmail.com>
2009-04-22 15:59:26 +0800

    opticstest: updated tests to work with new Jones matrices

--- physicspy/optics/tests/opticstest.py
+++ physicspy/optics/tests/opticstest.py
@@ -8,8 +8,7 @@ class TestSequenceFunctions(unittest.TestCase):
     
     def testRandomJonesNormalization(self):
         """ Normalize random Jones vector """
-        jones = JonesVector((1-random.rand())+(1-random.rand())*1j,(1-random.rand())++(1-random.rand())*1j)
-        #~ print self.jones.toArray()
+        jones = JonesVector(array([(1-random.rand())+(1-random.rand())*1j,(1-random.rand())++(1-random.rand())*1j]))
         jones = jones.normalize()
         self.failUnless(allclose(jones.size(),1.0), 'wrong Jones vector normalization')
 
@@ -17,7 +16,7 @@ class TestSequenceFunctions(unittest.TestCase):
         """ Convert random Jones vector to array """
         x = (1-random.rand())-(1-random.rand())*1j
         y = (1-random.rand())+(1-random.rand())*1j
-        jones = JonesVector(x,y)
+        jones = JonesVector(array([x,y]))
         self.failUnless(allclose(jones.toArray(),array([x,y])), 'wrong Jones vector to array conversion')
 
 if __name__ == '__main__':
